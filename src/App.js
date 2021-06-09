f13ecdebc48ad8ea891e717d0ac9cf6794cfa813
Nicolas Bermejo <nicbermejo@gmail.com>
2021-06-09 11:42:09 +0100

    styling the navbar

--- src/App.js
+++ src/App.js
@@ -1,35 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import './index.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { Breadcrumb } from 'react-bootstrap'
 
 function App() {
   return (
-    <html lang="en">
-      <head>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous"></link>
-      </head>
-      <body>
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Welcome to Bermejo Simulations!
-            </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          <div> 
-            <p> This is an additional slot </p>
-          </div>
+        <div>
+          <nav class="navbar navbar" style={NavbarStyle}>
+              <div class="container-fluid">
+                <span class="navbar-brand" style={{ color: "#F64C72" }}>Bermejo Simulations</span>
+                <Breadcrumb>
+                  <Breadcrumb.Item active style={{ color: "#FFFFFF" }}>Home</Breadcrumb.Item>
+                  <Breadcrumb.Item style={{ color: "#FFFFFF" }}>About</Breadcrumb.Item>
+                </Breadcrumb>
+              </div>
+          </nav>
+          <header className="App-header" style={BackgroundDiv}>
+
           </header>
         </div>
-      </body>
-    </html>
   );
+}
+
+const BackgroundDiv={
+  backgroundColor: "#242582"
+}
+
+const NavbarStyle={
+  backgroundColor: "#242582"
+}
+
+const Logo={
+  width: 30,
+  height: 30,
+
 }
 
 export default App;
