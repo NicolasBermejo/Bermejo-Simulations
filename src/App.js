8cf9196d9a3b76506f3f596a6de1901146adc58d
Nicolas Bermejo <nicbermejo@gmail.com>
2021-06-10 11:27:58 +0100

    creating custom navbar

--- src/App.js
+++ src/App.js
@@ -1,44 +1,21 @@
 import './App.css';
 import './index.js';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Background from "./img/background4.png"
 
-import { Breadcrumb } from 'react-bootstrap'
 
 function App() {
   return (
-        <div>
-          <nav class="navbar navbar" style={NavbarStyle}>
-              <div class="container-fluid">
-                <span class="navbar-brand" style={{ color: "rgb(36 68 82)", fontFamily: "impact", fontSize: 25 }}>Bermejo</span>
-                <Breadcrumb>
-                  <Breadcrumb.Item active style={{ color: "#rgb(36 68 82)", fontFamily: "courier"}}>Home</Breadcrumb.Item>
-                  <Breadcrumb.Item style={{ color: "rgb(36 68 82)", fontFamily: "courier" }}>About</Breadcrumb.Item>
-                </Breadcrumb>
-              </div>
-          </nav>
-          <header className="App-header" style={BackgroundDiv}>
-
-          </header>
-        </div>
+        <>
+        <Router>
+          <Navbar />
+            <Switch>
+              <Route path='/' exact />
+            </Switch>
+        </Router>
+        </>
   );
-}
-
-const BackgroundDiv={
-  backgroundImage: "linear-gradient(#242582, #5750AF)",
-  backgroundPosition: 'center',
-  backgroundSize: 'cover',
-  backgroundRepeat: 'no-repeat',
-}
-
-const NavbarStyle={
-  backgroundColor: "#242582"
-}
-
-const Logo={
-  width: 30,
-  height: 30,
-
 }
 
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,22 @@
+import React, {useState} from 'react';
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+    const {click, setClick} = useState(false)
+    return (
+        <>
+            <nav className="navbar">
+                <div className="navbar-container">
+                    <Link to="/" className="navbar-logo">
+                        SMLT <i class="fas fa-tint"></i>
+                    </Link>
+                    <div className='menu-icon' onClick={handleClick}> 
+                        <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+                    </div>
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default Navbar
