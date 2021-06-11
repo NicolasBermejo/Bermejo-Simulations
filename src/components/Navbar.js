033c9cf9d262b2c8bbe91ec1f9aab4e3c06f5ac9
Nicolas Bermejo <nicbermejo@gmail.com>
2021-06-11 08:54:06 +0100

    burger menu switch fixed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,26 @@
-import React, {useState} from 'react';
+import React,{ useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function Navbar() {
-    const {click, setClick} = useState(false)
+    const [click, setClick] = useState(false);
+
+    const handleClick = () => setClick(!click);
+
     return (
         <>
             <nav className="navbar">
                 <div className="navbar-container">
                     <Link to="/" className="navbar-logo">
-                        SMLT <i class="fas fa-tint"></i>
+                        SMLT <i class="fas fa-dice-d20"></i>
                     </Link>
                     <div className='menu-icon' onClick={handleClick}> 
                         <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
                     </div>
+                    <ul className={click ? 'nav-menu active' : 'nav-menu'}></ul>
                 </div>
             </nav>
         </>
-    )
+    );
 }
 
 export default Navbar
