b6be92f4e06823d7eebfbd6d0dbace86ef115218
Nicolas Bermejo <nicbermejo@gmail.com>
2021-06-14 14:33:08 +0100

    encompassed the cards img in div

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -7,8 +7,10 @@ function CardItem(props) {
             <li className='cards__item'> 
                 <a className='cards__item__link' href={props.href}>
                     <figure className='cards__item__pic-wrap' data-category={props.label}>
+                        <div className='cards__item__imgdiv'>
                         <img src={props.src} alt='Image' className='cards__item__img'>
                         </img>
+                        </div>
                         <div className='cards__item__info'>
                             <h5 className='cards__item__text'>{props.text}</h5>
                         </div>
