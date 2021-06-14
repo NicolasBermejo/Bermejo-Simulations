217728eb5fea040754ac1dd2298875c8d49617ef
Nicolas Bermejo <nicbermejo@gmail.com>
2021-06-14 11:52:10 +0100

    fixed images showing for cards

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -5,15 +5,21 @@ import './Cards.css'
 function Cards() {
     return (
         <div className='cards'>
-            <h1>Check these out for more info!</h1>
+            <h1>What you need to know</h1>
             <div className='cards__container'>
                 <div className='cards__wrapper'>
                     <ul className='cards__items'>
                         <CardItem 
-                            src='./img/tax-heaven.jpg'
+                            src='images/tax-heaven.jpg'
                             text='La fiscalité de la SASU : bénéfices, TVA, autres impôts et taxes'
-                            label='SASU'
+                            label='Fiscalité'
                             href= 'https://www.lecoindesentrepreneurs.fr/fiscalite-de-la-sasu/'
+                        />
+                        <CardItem 
+                            src='images/yellow-coins.jpg'
+                            text='Distribution de dividendes en SASU'
+                            label='Dividendes'
+                            href='https://agence-juridique.com/articles/comment-fonctionne-la-distribution-de-dividendes-en-sas'
                         />
                     </ul>
                 </div>
